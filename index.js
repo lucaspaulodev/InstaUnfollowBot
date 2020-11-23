fe15bb0cecdc52cd954a755417346d44f2e27d8d
Lucas Paulo <lucaspaulodev@gmail.com>
2020-11-23 04:15:22 -0300

    Adding comments

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ require('dotenv').config();
 const puppeteer = require('puppeteer-core');
 
 (async () => {
+  //Defining environment variables
   const USER = process.env.USER
   const PASSWORD = process.env.PASSWORD
 
+  //Setup browser pupperteer
   const browser = await puppeteer.launch({
     headless: false,
+    defaultViewport: {width: 1920, height: 1080},
     executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
   });
   
+  //Opening the browser
   const page = await browser.newPage();
+
+  //Loading page address
   await page.goto('https://instagram.com/');
   await page.waitForSelector('input[name="username"]')
+
+  //Typing instagram credentials to sign in
   await page.type('input[name="username"]', USER)
   await page.type('input[name="password"]', PASSWORD)
   await page.click('button[type="submit"]')
+
+  //Navigating within the profile
   await page.waitForSelector('#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.ctQZg > div > div:nth-child(5) > span')
   await page.click('#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.ctQZg > div > div:nth-child(5) > span')
   await page.waitForSelector('#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.ctQZg > div > div:nth-child(5) > div.poA5q > div.uo5MA._2ciX.tWgj8.XWrBI > div._01UL2 > a:nth-child(1)')
   await page.click('#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.ctQZg > div > div:nth-child(5) > div.poA5q > div.uo5MA._2ciX.tWgj8.XWrBI > div._01UL2 > a:nth-child(1)')
   await page.waitForSelector('.k9GMp')
 
-  
+  //Taking a screenshot of the profile screen
   await page.screenshot({path: 'me.png'})
 
-
+  //Closing the Browser
   await browser.close();
 })();
