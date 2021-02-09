2276431ee51e534455132a0c80d06d8669cc9b0f
Lucas Paulo <lucaspaulodev@gmail.com>
2021-02-09 18:24:27 -0300

    Improving experience

--- src/robots/UnfollowBot.js
+++ src/robots/UnfollowBot.js
@@ -5,36 +5,56 @@ async function UnfollowBot(nonMutualFollowers, page) {
     console.log('UNMUTUAL FOLLOWERS', nonMutualFollowers);
 
     if (nonMutualFollowers.length !== 0) {
-        var userOptions = ['Unfollow all non-mutual followers', 'Unfollow a specific quantity of non-mutual followers']
-        var index = readlineSync.keyInSelect(userOptions, 'Which option you whant?');
+        var userOptions = ['Unfollow 20 non-mutual followers', 'Unfollow 40 non-mutual followers', 'Unfollow 60 non-mutual followers']
+        var index = readlineSync.keyInSelect(userOptions, 'Which option you whant? (You can unfollow 160 profiles per hour in Instagram)');
 
         if (index === 0) {
             await page.evaluate((nonMutualFollowers) => {
-                nonMutualFollowers.forEach(nonMutualFollower => {
-                    var username = nonMutualFollower
+                for (var i = 0; i <= 20; i++) {
+                    var username = nonMutualFollowers[i]
                     var userTag = document.querySelector(`a[title="${username}"]`)
                     var userFather = userTag.closest('li')
                     var userButton = userFather.querySelector('button')
-
                     userButton.click()
 
                     var unfollowButton = document.querySelector('button.-Cab_')
 
                     unfollowButton.click()
-
                     setTimeout((username) => {
                         console.log(`Stop following ${username}`)
                     }, 2000)
-
-                });
+                }
             }, nonMutualFollowers)
 
             await browser.close();
         }
         else if (index === 1) {
-            var quantityToUnfollow = readlineSync.questionInt('How many do you want to stop following?\n')
-            await page.evaluate((nonMutualFollowers, quantityToUnfollow) => {
-                for (var i = 0; i <= quantityToUnfollow; i++) {
+            await page.evaluate((nonMutualFollowers) => {
+                for (var i = 0; i <= 40; i++) {
+                    var username = nonMutualFollowers[i]
+                    var userTag = document.querySelector(`a[title="${username}"]`)
+                    var userFather = userTag.closest('li')
+                    var userButton = userFather.querySelector('button')
+                    userButton.click()
+
+                    var unfollowButton = document.querySelector('button.-Cab_')
+
+                    unfollowButton.click()
+                    setTimeout((username) => {
+                        console.log(`Stop following ${username}`)
+                    }, 2000)
+                }
+            }, nonMutualFollowers, quantityToUnfollow)
+
+            console.log('You unfollow this users:\n')
+            for (var i = 0; i < quantityToUnfollow; i++) {
+                console.log(nonMutualFollowers[i])
+            }
+            await browser.close();
+        }
+        else if (index === 2) {
+            await page.evaluate((nonMutualFollowers) => {
+                for (var i = 0; i <= 60; i++) {
                     var username = nonMutualFollowers[i]
                     var userTag = document.querySelector(`a[title="${username}"]`)
                     var userFather = userTag.closest('li')
@@ -60,7 +80,6 @@ async function UnfollowBot(nonMutualFollowers, page) {
             console.log("You exited from the robot")
             await browser.close();
         }
-
     }
     else {
         console.log("You don´t have non-mutual followers!!")
